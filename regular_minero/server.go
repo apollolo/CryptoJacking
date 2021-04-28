6228aa8a3363395899acbf9130c9b9817155068f
Apollo Lo <apollol@bu.edu>
2021-04-28 02:00:06 -0400

    regular mining

---
+++ regular_minero/server.go
@@ -0,0 +1,9 @@
+package main
+import (
+        "log"
+        "net/http"
+)
+func main() {
+    http.Handle("/", http.FileServer(http.Dir("./static")))
+    log.Fatal(http.ListenAndServe(":8081", nil))
+}
